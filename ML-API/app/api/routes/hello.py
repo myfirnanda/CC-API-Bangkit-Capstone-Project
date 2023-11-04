f8bc0c94d8d7114c14e26abd294159afc7ce509f
fathur-rs <fathurrahmansyarief@gmail.com>
2023-11-04 21:58:36 +0700

    add ML-API folder & fastAPI boilerplate project code

---
+++ ML-API/app/api/routes/hello.py
@@ -0,0 +1,8 @@
+# app/api/routes/hello.py
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/hello")
+def read_hello():
+    return {"message": "Hello, World"}
---
+++ ML-API/app/core/config.py
@@ -0,0 +1,4 @@
+# app/core/config.py
+class Config:
+    PROJECT_NAME = "boilerplate"
+    DEBUG = True
